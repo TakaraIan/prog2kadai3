ece15eaf002c54ec54b2141a27f1fd7b6467ff04
Ian TAKARA <e245718@ie.u-ryukyu.ac.jp>
2024-12-02 09:42:57 +0900

    4th commit in total

--- app/src/main/java/jp/ac/uryukyu/ie/e245718/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245718/Enemy.java
@@ -1,43 +1,13 @@
-
 package jp.ac.uryukyu.ie.e245718;
 
-public class Enemy {
-    private String name;
-    private int hitPoint;
-    private int attack;
-    private boolean dead;
+public class Enemy extends LivingThing {
 
-    public Enemy(String name, int maximumHP, int attack) {
-        this.name = name;
-        this.hitPoint = maximumHP;
-        this.attack = attack;
-        this.dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    public Enemy(String name, int hitPoint, int attack) {
+        super(name, hitPoint, attack);  // 親クラスのコンストラクタを呼び出し
     }
 
-    public void attack(Hero h) {
-        int damage = (int) (Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, h.getName(), damage);
-        h.wounded(damage);
-    }
-
-    public void wounded(int damage) {
-        hitPoint -= damage;
-        if (hitPoint <= 0) {
-            dead = true;
-            System.out.printf("%sは倒れた。\n", name);
-        }
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getHitPoint() {
-        return hitPoint;
-    }
-
-    public boolean isDead() {
-        return dead;
+    @Override
+    public void woundedMessage() {
+        System.out.printf("モンスター%sは倒れた。\n", getName());
     }
 }
--- app/src/main/java/jp/ac/uryukyu/ie/e245718/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245718/Hero.java
@@ -1,42 +1,13 @@
 package jp.ac.uryukyu.ie.e245718;
 
-public class Hero {
-    private String name;
-    private int hitPoint;
-    private int attack;
-    private boolean dead;
+public class Hero extends LivingThing {
 
-    public Hero(String name, int maximumHP, int attack) {
-        this.name = name;
-        this.hitPoint = maximumHP;
-        this.attack = attack;
-        this.dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    public Hero(String name, int hitPoint, int attack) {
+        super(name, hitPoint, attack);
     }
 
-    public void attack(Enemy e) {
-        int damage = (int) (Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
-        e.wounded(damage);
-    }
-
-    public void wounded(int damage) {
-        hitPoint -= damage;
-        if (hitPoint <= 0) {
-            dead = true;
-            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
-        }
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getHitPoint() {
-        return hitPoint;
-    }
-
-    public boolean isDead() {
-        return dead;
+    @Override
+    public void woundedMessage() {
+        System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
     }
 }
